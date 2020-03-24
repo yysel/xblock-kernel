612f7baadde41eac8592ff4e20091927eba5f762
纪猛 <927994432@qq.com>
2020-03-25 03:43:09 +0800

    提高路由注册的时机

--- src/KernelProvider.php
+++ src/KernelProvider.php
@@ -11,18 +11,12 @@ class KernelProvider extends ServiceProvider
     public function boot()
     {
         $this->app->configure('xblock');
+        $this->registerRouter();
     }
 
     public function register()
     {
-
         $this->registerMigrations();
-        $this->app->router->group(['prefix' => config('xblock.prefix', 'api/xblock'), 'namespace' => 'XBlock\Kernel', 'middleware' => config('xblock.middleware', 'auth:api')], function ($router) {
-            $router->post('/menu', 'KernelController@menu');
-            $router->post('/notification', 'KernelController@notification');
-            $router->post('/{block}/{action}', 'BlockController@action');
-        });
-
         $this->app->singleton('field_dict', function () {
             $register = config('xblock.register.dict', false);
             return $register ? new $register : null;
@@ -34,5 +28,16 @@ class KernelProvider extends ServiceProvider
         return $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 
+    protected function registerRouter()
+    {
+        $this->app->router->group([
+            'prefix' => config('xblock.prefix', 'api/xblock'),
+            'namespace' => 'XBlock\Kernel', 'middleware' => config('xblock.middleware', 'auth:api')
+        ], function ($router) {
+            $router->post('/menu', 'KernelController@menu');
+            $router->post('/notification', 'KernelController@notification');
+            $router->post('/{block}/{action}', 'BlockController@action');
+        });
+    }
 
 }
